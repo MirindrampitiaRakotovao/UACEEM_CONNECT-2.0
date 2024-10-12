3ca839d146453e7a11f6388bbe04084975d399a1
FaniriniainaLucia <tolotriniavofaniriniainalucia@gmail.com>
2024-10-12 20:35:58 +0200

    voila un peu

--- src/components/ModalFile.tsx
+++ src/components/ModalFile.tsx
@@ -15,9 +15,9 @@ const ModalFile: React.FC<ModalFileProps> = ({ isOpen, onClose, files }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       {/* Contenu du modal */}
-      <div className="relative bg-white rounded-lg shadow-lg max-w-xl mx-auto w-full h-[90vh] overflow-y-auto">
+      <div className="relative bg-white rounded-lg shadow-lg max-w-xl mx-auto w-full max-h-[90vh] overflow-y-auto">
         {/* Bouton de fermeture */}
         <div className="flex justify-between items-center mb-4">
           <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
