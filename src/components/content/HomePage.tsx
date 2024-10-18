8c09fd0fe817c626e1566e3831dc98e392fce849
FaniriniainaLucia <tolotriniavofaniriniainalucia@gmail.com>
2024-10-18 08:45:23 +0200

    home page

--- src/components/content/HomePage.tsx
+++ src/components/content/HomePage.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import SidebarLeft from './SidebarLeft';
 import SidebarRight from './SidebarRight';
 import MainContent from './MainContent';
+import { useDarkMode } from '../../contexts/DarkModeContext'; // Import the useDarkMode hook
 
 const HomePage: React.FC = () => {
+  const { isDarkMode } = useDarkMode(); // Get the dark mode state
+
   return (
-    <div className="flex bg-gray-100 overflow-hidden">
+    <div className={`flex overflow-hidden ${isDarkMode ? 'bg-gray-900' : 'bg-gray-100'} dark:bg-gray-900`}>
       {/* Sidebar gauche */}
       <div className="flex-none w-1/3 h-full">
         <SidebarLeft />
       </div>
 
       {/* Contenu principal avec scrollbar invisible */}
-      <div className="flex-grow  h-full  ">
+      <div className="flex-grow h-full">
         <MainContent />
       </div>
 
       {/* Sidebar droit */}
-      <div className="flex-none w-1/3  h-full">
+      <div className="flex-none w-1/3 h-full">
         <SidebarRight />
       </div>
     </div>
